e9c3b31c8656e3a00a77c14a06be48407349f4b0
sjungmin98 <shc980@gmail.com>
2024-01-08 15:23:43 +0900

    123

--- databases/connections.py
+++ databases/connections.py
@@ -8,21 +8,13 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # DATABASE_URL: Optional[str] = None
-    DATABASE_URL : Optional[str]  # mongodb 연결
-    # async-await == 네크워크의 속도와 맞추기 위해 넣는 기능(방식은 비동기)
+    DATABASE_URL : Optional[str]
+
     async def initialize_database(self,):
         client = AsyncIOMotorClient(self.DATABASE_URL)
         await init_beanie(database=client.get_default_database(),
-                          document_models=[User]) # collection 리스트
-    async def initialize_database(self,):
-        client = AsyncIOMotorClient(self.DATABASE_URL)
-        await init_beanie(database=client.get_default_database(),
-                          document_models=[Question]) # collection 리스트
-    async def initialize_database(self,):
-        client = AsyncIOMotorClient(self.DATABASE_URL)
-        await init_beanie(database=client.get_default_database(),
-                          document_models=[Answer]) # collection 리스트
+                          document_models=[User, Question, Answer])
+
     class Config:
         env_file = ".env"
 
